7410daf262d3cb2d9d3b3bab1a523a870f11393a
Ubuntu <ubuntu@ip-172-31-34-62.ap-northeast-3.compute.internal>
2024-05-21 20:08:49 +0000

    update

--- tuner.config.js
+++ tuner.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  // RPCProviderUrl: process.env.PUBLIC_MAINNET_PROVIDER_URL,
-  // PrivateRPCProviderUrl: process.env.MAINNET_PROVIDER_URL,
-  RPCProviderUrl: "http://127.0.0.1:8489",
-  PrivateRPCProviderUrl: "http://127.0.0.1:8489",
+  RPCProviderUrl: process.env.PUBLIC_MAINNET_PROVIDER_URL,
+  PrivateRPCProviderUrl: process.env.MAINNET_PROVIDER_URL,
   IPCProviderUrl: process.env.IPC_PROVIDER_URL,
   providerToBeUsed: "privateRpc" // privateRpc | privateIpc | public
 };
